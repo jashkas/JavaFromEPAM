0f7eb3772347e16fcbc3924ebdb8ad91965d796f
jashkas <jyli.jl.jl@gmail.com>
2024-12-09 19:53:33 +0800

    Глава 3. Вариант A. исправления

--- src/chapter3/VariableA/PhoneArray.java
+++ src/chapter3/VariableA/PhoneArray.java
@@ -10,100 +10,75 @@ public class PhoneArray {
     private Phone[] phones;
 
     public PhoneArray() {
-        int size = 10;
-        phones = new Phone[size];
-        phones = createArray(size);
-    }
-    public PhoneArray(int count) {
-        // ограничено 10 записями
-        if (count > 0 && count < 10) {
-            phones = new Phone[count];
-            phones = createArray(count);
-        }
+        phones = createArray();
     }
     public PhoneArray(Phone[] phones) {
         this.phones = phones;
     }
     // возвращает массив сгенерированных объектов Phones
-    private Phone[] createArray(int count) {
+    private Phone[] createArray() {
         Random random = new Random();
-        count--;  // переданное число становится счетчиком
-        while (count >= 0) {
-            if (count == 9) {
+
+        phones = new Phone[]{
                 // инициализация объекта с передачей всех аргументов
                 // предполагается, что данный конструктор вызывается для инициализации известного объекта, например, прочитанного из базы данных,
                 // но для примера, аргументы передаются рандомные
-                phones[0] = new Phone(Phone.generateId(),
-                        "Смирнов",
-                        "Дмитрий",
-                        "Владимирович",
-                        "Россия, г. Чебоксары, Сельская ул., д. 11 кв.132",
-                        random.nextLong(Long.MAX_VALUE) + 1,
-                        (short) random.nextInt(1000),  // дебит до 1000
-                        (short) random.nextInt(1000),  // кредит до 1000
-                        0,  // междугородняя связь - не было пользования
-                        (long) random.nextInt(200 * 60) + 100 // городские разговоры менее 300 минут
-                );
-            }
-            else if (count == 8) {
+                new Phone(Phone.generateId(),
+                    "Смирнов",
+                    "Дмитрий",
+                    "Владимирович",
+                    "Россия, г. Чебоксары, Сельская ул., д. 11 кв.132",
+                    random.nextLong(Long.MAX_VALUE) + 1,
+                    (short) random.nextInt(1000),  // дебит до 1000
+                    (short) random.nextInt(1000),  // кредит до 1000
+                    0,  // междугородняя связь - не было пользования
+                    (long) random.nextInt(200 * 60) + 100 // городские разговоры менее 300 минут
+                ),
                 // инициализация с передачей ФИО, адреса и номера кредитной карты
                 // словно заполняются данные о новом абоненте
-                phones[1] = new Phone("Харитонов",
+                new Phone("Харитонов",
                         "Александр",
                         "Игоревич",
                         "Россия, г. Жуковский, Дорожная ул., д. 24 кв.161",
                         random.nextLong(Long.MAX_VALUE) + 1
-                );
-            }
-            else if (count == 7) {
-                // инициализация с передачей адреса и номера кредитной карты
-                phones[2] = new Phone("Рубцов Родион Тихонович", "Россия, г. Коломна, Колхозный пер., д. 24 кв.9",
-                        random.nextLong(Long.MAX_VALUE) + 1);
-                phones[2].setLongDistanceTimeConversation(0);
-                phones[2].setTownTimeConversation(98 * 60);
-            }
-            else if (count == 6) {
-                // инициализация пустого объекта
-                int third = 3;
-                phones[third] = new Phone();
-                // установка значений в пустом объекте через сеттеры
-                phones[third].setSurname("Максимова");
-                phones[third].setName("Мария");
-                phones[third].setPatronymic("Михайловна");
-                phones[third].setAddress("Россия, г. Ставрополь, Дружная ул., д. 7 кв.144");
-                phones[third].setCreditCardNumber(random.nextLong(Long.MAX_VALUE) + 1);
-                phones[third].setDebit((short) random.nextInt(1000));  // дебит до 1000;
-                phones[third].setCredit((short) random.nextInt(1000));  // кредит до 1000;
-                phones[third].setLongDistanceTimeConversation((long) random.nextInt(300 * 60) + 100);  // междугородние разговоры до 300 минут
-                phones[third].setTownTimeConversation(200 * 60);  // городские разговоры 200
-            }
-            else if (count == 5) {
+                ),
                 // инициализация с вызовом конструктора фио одной строкой
-                phones[4] = new Phone("Соколова Екатерина Дмитриевна", "Россия, г. Рязань, Почтовая ул., д. 24 кв.86", random.nextLong(Long.MAX_VALUE) + 1);
-                phones[4].setTownTimeConversation(150 * 60);
-            }
-            else if (count == 4) {
-                // использование конструктора, который имитирует добавление информации о новом абоненте
-                phones[5] = new Phone("Прокофьев", "Илья", "Макарович", "Россия, г. Череповец, Дружбы ул., д. 17 кв.198", random.nextLong(Long.MAX_VALUE) + 1);
-            }
-            else if (count == 3) {
-                phones[6] = new Phone("Носова", "Елизавета", "Максимовна", "Россия, г. Бийск, Пролетарская ул., д. 25 кв.70", random.nextLong(Long.MAX_VALUE) + 1);
-                phones[6].setLongDistanceTimeConversation(100 * 60);
-            }
-            else if (count == 2) {
-                phones[7] = new Phone("Егорова", "София", "Львовна", "Россия, г. Воронеж, Садовый пер., д. 21 кв.126", random.nextLong(Long.MAX_VALUE) + 1);
-            }
-            else if (count == 1) {
-                phones[8] = new Phone("Чернышев", "Максим", "Вячеславович", "Россия, г. Димитровград, Восточная ул., д. 7 кв.139", random.nextLong(Long.MAX_VALUE) + 1);
-                phones[2].setTownTimeConversation(50 * 60);
-            }
-            else if (count == 0) {
-                phones[9] = new Phone("Лазарев", "Фёдор", "Маркович", "Россия, г. Норильск, Вокзальная ул., д. 21 кв.193", random.nextLong(Long.MAX_VALUE) + 1);
-                phones[9].setLongDistanceTimeConversation(10 * 60);
-            }
+                new Phone("Рубцов Родион Тихонович", "Россия, г. Коломна, Колхозный пер., д. 24 кв.9",
+                        random.nextLong(Long.MAX_VALUE) + 1
+                ),
+                // инициализация пустого объекта
+                new Phone(),
+                // инициализация с вызовом конструктора фио одной строкой
+                new Phone("Соколова Екатерина Дмитриевна", "Россия, г. Рязань, Почтовая ул., д. 24 кв.86", random.nextLong(Long.MAX_VALUE) + 1),
+                new Phone("Прокофьев", "Илья", "Макарович", "Россия, г. Череповец, Дружбы ул., д. 17 кв.198", random.nextLong(Long.MAX_VALUE) + 1),
+                new Phone("Носова", "Елизавета", "Максимовна", "Россия, г. Бийск, Пролетарская ул., д. 25 кв.70", random.nextLong(Long.MAX_VALUE) + 1),
+                new Phone("Егорова", "София", "Львовна", "Россия, г. Воронеж, Садовый пер., д. 21 кв.126", random.nextLong(Long.MAX_VALUE) + 1),
+                new Phone("Чернышев", "Максим", "Вячеславович", "Россия, г. Димитровград, Восточная ул., д. 7 кв.139", random.nextLong(Long.MAX_VALUE) + 1),
+                new Phone("Лазарев", "Фёдор", "Маркович", "Россия, г. Норильск, Вокзальная ул., д. 21 кв.193", random.nextLong(Long.MAX_VALUE) + 1)
+        };
 
-            count--;  // уменьшение счетчика записей
-        }
+        int third = 3;
+        // установка значений в пустом объекте через сеттеры
+        phones[third].setSurname("Максимова");
+        phones[third].setName("Мария");
+        phones[third].setPatronymic("Михайловна");
+        phones[third].setAddress("Россия, г. Ставрополь, Дружная ул., д. 7 кв.144");
+        phones[third].setCreditCardNumber(random.nextLong(Long.MAX_VALUE) + 1);
+        phones[third].setDebit((short) random.nextInt(1000));  // дебит до 1000;
+        phones[third].setCredit((short) random.nextInt(1000));  // кредит до 1000;
+        phones[third].setLongDistanceTimeConversation((long) random.nextInt(300 * 60) + 100);  // междугородние разговоры до 300 минут
+        phones[third].setTownTimeConversation(200 * 60);  // городские разговоры 200
+
+        // городские разговоры
+        phones[2].setTownTimeConversation(98 * 60);
+        phones[4].setTownTimeConversation(150 * 60);
+        phones[8].setTownTimeConversation(50 * 60);
+
+        // междугородние разговоры
+        phones[2].setLongDistanceTimeConversation(0);
+        phones[6].setLongDistanceTimeConversation(100 * 60);
+        phones[9].setLongDistanceTimeConversation(10 * 60);
+
         return phones;
     }
 
--- src/chapter3/VariableA/PhoneMain.java
+++ src/chapter3/VariableA/PhoneMain.java
@@ -14,49 +14,53 @@ public class PhoneMain {
         if (optionalPhones.isPresent()) {
             Phone[] phoneArray = optionalPhones.get();  // получение массива из Optional
             System.out.println("a. Сведения об абонентах, у которых время внутригородских разговоров превышает " + limit);
-//            PhoneArray subscribersWithOverLimit = new PhoneArray(phoneArray);
-//            System.out.println(subscribersWithOverLimit);
-            final String BLANK = ";";
-            StringBuilder str = new StringBuilder();
-            str.append("Телефон;ФИО;Минуты внутригородских разговоров").append("\n");
-            for (Phone phone: phoneArray) {
-                str.append("+").append(phone.getId()).append(BLANK);
-                str.append(phone.getSurname()).append(" ");
-                str.append(phone.getName()).append(" ");
-                str.append(phone.getPatronymic()).append(BLANK);
-                str.append(phone.getTownTimeConversation()).append("\n");
-            }
-            System.out.println(str);
+            PhoneArray subscribersWithOverLimit = new PhoneArray(phoneArray);
+            System.out.println(subscribersWithOverLimit);
+//            final String BLANK = ";";
+//            StringBuilder str = new StringBuilder();
+//            str.append("Телефон;ФИО;Минуты внутригородских разговоров").append("\n");
+//            for (Phone phone: phoneArray) {
+//                str.append("+").append(phone.getId()).append(BLANK);
+//                str.append(phone.getSurname()).append(" ");
+//                str.append(phone.getName()).append(" ");
+//                str.append(phone.getPatronymic()).append(BLANK);
+//                str.append(phone.getTownTimeConversation()).append("\n");
+//            }
+//            System.out.println(str);
         }
 
         optionalPhones = phones.findSubscribersWithLongDistance();
         if (optionalPhones.isPresent()) {
             Phone[] phoneArray = optionalPhones.get();  // получение массива из Optional
             System.out.println("b. Сведения об абонентах, которые пользовались междугородной связью");
-            final String BLANK = ";";
-            StringBuilder str = new StringBuilder();
-            str.append("Телефон;ФИО;Минуты междугородней связи").append("\n");
-            for (Phone phone: phoneArray) {
-                str.append("+").append(phone.getId()).append(BLANK);
-                str.append(phone.getSurname()).append(" ");
-                str.append(phone.getName()).append(" ");
-                str.append(phone.getPatronymic()).append(BLANK);
-                str.append(phone.getLongDistanceTimeConversation()).append("\n");
-            }
-            System.out.println(str);
+            PhoneArray subscribersWithLongDistance = new PhoneArray(phoneArray);
+            System.out.println(subscribersWithLongDistance);
+//            final String BLANK = ";";
+//            StringBuilder str = new StringBuilder();
+//            str.append("Телефон;ФИО;Минуты междугородней связи").append("\n");
+//            for (Phone phone: phoneArray) {
+//                str.append("+").append(phone.getId()).append(BLANK);
+//                str.append(phone.getSurname()).append(" ");
+//                str.append(phone.getName()).append(" ");
+//                str.append(phone.getPatronymic()).append(BLANK);
+//                str.append(phone.getLongDistanceTimeConversation()).append("\n");
+//            }
+//            System.out.println(str);
         }
 
         System.out.println("c. Сведения об абонентах в алфавитном порядке");
         Phone[] sorted = phones.sortSubscribers();
-        final String BLANK = ";";
-        StringBuilder str = new StringBuilder();
-        str.append("ФИО;Номер кредитной карты").append("\n");
-        for (Phone phone: sorted) {
-            str.append(phone.getSurname()).append(" ");
-            str.append(phone.getName()).append(" ");
-            str.append(phone.getPatronymic()).append(BLANK);
-            str.append(phone.getCreditCardNumber()).append("\n");
-        }
-        System.out.println(str);
+        PhoneArray sortedSubscribers = new PhoneArray(sorted);
+        System.out.println(sortedSubscribers);
+//        final String BLANK = ";";
+//        StringBuilder str = new StringBuilder();
+//        str.append("ФИО;Номер кредитной карты").append("\n");
+//        for (Phone phone: sorted) {
+//            str.append(phone.getSurname()).append(" ");
+//            str.append(phone.getName()).append(" ");
+//            str.append(phone.getPatronymic()).append(BLANK);
+//            str.append(phone.getCreditCardNumber()).append("\n");
+//        }
+//        System.out.println(str);
     }
 }
